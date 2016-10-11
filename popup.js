f3acf5d7cce7876126215c73162faec5c2f88e10
Brian Tang <brtang@ucsc.edu>
2016-10-10 19:04:47 -0700

    popup.js is able to store inputs from popup.html locally and launch new tab from input. Still need to validate input

--- popup.js
+++ popup.js
@@ -1,1 +1,27 @@
-chrome.tabs.create({"url": "http://google.com"});
+
+var but = document.getElementById("button");
+if(but){
+but.addEventListener('click', saveChanges);
+}
+
+function saveChanges(){
+    console.log("This works\n");
+    var ts = document.getElementById("tabspace").value;
+    var sitename = document.getElementById("website").value;
+    console.log(ts);
+    
+    chrome.storage.local.set({'ts': ts});
+    chrome.storage.local.set({'sitename': sitename});
+   /* var dataObj = {};
+    dataObj[test] = ts;
+    dataObj[test2] = sitename;
+    
+    storage.set(dataObj);*/
+    var string = "";
+    chrome.storage.local.get('ts', function(result){
+        string = result.ts;
+        document.getElementById("demo").innerHTML = string;
+        chrome.tabs.create({active: true, url: string});
+    });
+    
+}
