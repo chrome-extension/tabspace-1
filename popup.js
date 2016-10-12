e6f30495649c2ed387b021d5592a173a47c2fcf1
Brian Tang <brtang@ucsc.edu>
2016-10-12 15:20:51 -0700

    Added code to check if input has http:// and concat strings if it does not

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-/*------Tabspace class ----------*/
+/*------Tabspace class ----------
 function Tabspace(){
     
 }
@@ -13,6 +13,8 @@ Tabspace.prototype = {
     }
 }
 
+*/
+
 /*Add on click listener to button*/
 var bttn = document.getElementById("button");
 if(bttn){
@@ -21,29 +23,37 @@ bttn.addEventListener('click', saveChanges);
 
 /*On click function to save form inputs*/
 function saveChanges(){
-    var substring = "https://"
-
+    var substring = "https://";
+    var fullSitename = "";
+    
     console.log("This works\n");
     var ts = document.getElementById("tabspace").value;
     var sitename = document.getElementById("website").value;
-    console.log(ts);
+    console.log(sitename);
     
-    if(sitename.indexOf(substring) !== -1){
-        /*append substring code TDL*/
+    if(sitename.includes(substring) !== true){
+        console.log("No https string");
+        fullSiteName = substring.concat(sitename);
+        console.log(fullSiteName);
+    }else{
+        fullSiteName = sitename;
+        console.log(fullSiteName);
     }
     
     chrome.storage.local.set({'ts': ts});
-    chrome.storage.local.set({'sitename': sitename});
+    chrome.storage.local.set({'sitename': fullSiteName});
    /* var dataObj = {};
     dataObj[test] = ts;
     dataObj[test2] = sitename;
     
     storage.set(dataObj);*/
-    var string = "";
+    
+    var site = "";
     chrome.storage.local.get(function(result){
         tabspace = result.ts;
         site = result.sitename;
         document.getElementById("demo").innerHTML = tabspace;
+        console.log(site);
         chrome.tabs.create({active: true, url: site});
     });
     
