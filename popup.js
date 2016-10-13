b4a8300bdd8a0cd63ce78be3808a84423b01a854
ewwinnie <ewwinnie@gmail.com>
2016-10-12 19:57:42 -0700

    Created button to open a new window and a button to close the current window and open a new one

--- popup.js
+++ popup.js
@@ -1,1 +1,22 @@
-chrome.tabs.create({"url": "http://google.com"});
+// Open tabspace
+// get reference to element
+var openWindowBtn = document.getElementById('openWindow');
+
+openWindowBtn.addEventListener('click', function(event) {
+    chrome.windows.create({"url": "http://google.com"});
+});
+
+// Close current tabspace and open new tabspace
+// get reference to element
+//chrome.tabs.query({'active': true}, function(tabs) {
+    //chrome.tabs.remove(tabs[0].id);
+
+var switchWindowBtn = document.getElementById('switchWindow');
+
+switchWindowBtn.addEventListener('click', function(event) {
+    chrome.windows.getCurrent(function(currentWindow){
+        chrome.windows.remove(currentWindow.id);
+        chrome.windows.create({"url": "http://google.com"});
+    });
+});
+    
