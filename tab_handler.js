b8229f658d3177cc8a82477a12bbaad619c7636b
Kevin Pannell <pannellk69@Kevins-MacBook-Pro.local>
2016-10-13 17:25:17 -0700

    figuring out js

--- tab_handler.js
+++ tab_handler.js
@@ -1,12 +1,30 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
-function load_tabspace(){
-	alert("we in here");
+// function load_tabspace(){
+// 	alert("we in here");
+// 	chrome.tabs.query(currentWindow, function(arrayOfTabs){
+// 		var tab = arrayOfTabs[0];
+// 		var url = tab.url;
+// 		window.alert(url);
+// 	});
+// }
+
+// });
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("save_tabspace").addEventListener("click", save_tabspace);
+});
+
+console.log("a;slkdfj;");
+
+function save_tabspace(){
+	
+	console.log("we in here");
+
+
 	chrome.tabs.query(currentWindow, function(arrayOfTabs){
 		var tab = arrayOfTabs[0];
 		var url = tab.url;
-		window.alert(url);
+		alert(url);
 	});
 }
-
-});
