a58d50a325cfd5849073da7883569e1b763fca0d
Cameron Wright <clw@camlewwri.com>
2016-10-17 17:40:13 -0700

    content.js for blacklist

--- content.js
+++ content.js
@@ -1,4 +1,12 @@
-//get tabspaces from chrome.storage
+var tabspace;
+chrome.storage.sync.get('tabspace', function (tabspace) {
+	console.log(tabspace);
+});
 
-
-
+for (var i = 0; i < tabspace.blacklisted.length(); i++) {
+	var re = new RegExp(tabspace.blacklisted[i]);
+	
+	if (re.test(window.location)) {
+		window.location = "http://camlewwri.com/tabspace/block-page.html";
+	}
+}
