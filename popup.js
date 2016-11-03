44ebf31d288ddeee3cb0cbb3fa2120c4ca45e404
ewwinnie <ewwinnie@gmail.com>
2016-11-03 13:54:35 -0700

    updated block tabs list; works now

--- popup.js
+++ popup.js
@@ -7,26 +7,56 @@ window.onload = function(){
         event.preventDefault();
         if (event.keyCode == 13 && addValue.value != "") {
             // Add tab url
-            var node = document.createElement("li");
-            var textnode = document.createTextNode(addValue.value);
-            node.appendChild(textnode);
-            document.getElementById("list-add").appendChild(node);
+            var nodeAdd = document.createElement("li");
+            var textnodeAdd = document.createTextNode(addValue.value);
+            nodeAdd.appendChild(textnodeAdd);
+            document.getElementById("list-add").appendChild(nodeAdd);
             addValue.value = "";
         }
         else {
             return false;
         }
 
+
         // Remove tab url
-        var removeTask = document.createElement('input');
-        removeTask.setAttribute('type', 'button');
-        removeTask.setAttribute("value", "x");
-        removeTask.setAttribute("id", "removeButton");
-        removeTask.addEventListener('click', function(e) {
-            node.parentNode.removeChild(node);
+        var removeAddTab = document.createElement('input');
+        removeAddTab.setAttribute('type', 'button');
+        removeAddTab.setAttribute("value", "x");
+        removeAddTab.setAttribute("id", "removeButton");
+        removeAddTab.addEventListener('click', function(e) {
+            nodeAdd.parentNode.removeChild(nodeAdd);
         }, false);
-        node.appendChild(removeTask);
+        nodeAdd.appendChild(removeAddTab);
     }
     ) 
 
+    // BLOCK TABS
+    // Enter key is pressed
+    blockValue = document.getElementById("textbox-block");
+    blockValue.addEventListener("keyup", function(event){
+        event.preventDefault();
+        if (event.keyCode == 13 && blockValue.value != "") {
+            // Add tab url
+            var nodeBlock = document.createElement("li");
+            var textnodeBlock = document.createTextNode(blockValue.value);
+            nodeBlock.appendChild(textnodeBlock);
+            document.getElementById("list-block").appendChild(nodeBlock);
+            blockValue.value = "";
+        }
+        else {
+            return false;
+        }
+
+
+        // Remove tab url
+        var removeBlockTab = document.createElement('input');
+        removeBlockTab.setAttribute('type', 'button');
+        removeBlockTab.setAttribute("value", "x");
+        removeBlockTab.setAttribute("id", "removeButton");
+        removeBlockTab.addEventListener('click', function(e) {
+            nodeBlock.parentNode.removeChild(nodeBlock);
+        }, false);
+        nodeBlock.appendChild(removeBlockTab);
+    }
+    ) 
 };
