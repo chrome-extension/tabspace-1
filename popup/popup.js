854425e9b2e3d8b383a90a1b4521f452bd3c09a8
kapn <pannellk69@yahoo.com>
2016-11-23 13:31:47 -0800

    adjusted repo to be how we wanted it, now just drag tabspace directory for extension installation instead of kp-ts as before

--- popup/popup.js
+++ popup/popup.js
@@ -5,11 +5,16 @@ function Tabspace(theName){
     this.blocksArray = [];
 }
 
+function sanitize(str) {
+    //regex to find & < > characters and replace them with html values
+    return str.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;');
+}
+
 /*-On Click function to append a new Tabspace object to global array and save into chrome.storage 
 Invokes displayButtons function afterwards to update button display on home page----------*/
 function saveTS(){
-    var substring = "https://";
-    var name = document.getElementById("textbox-name").value;
+    var substring = "http://";
+    var name = sanitize(document.getElementById("textbox-name").value);
     tabspace = new Tabspace(name);
     $("#list-add li").each(function() { 
         var fullSiteName = "";
@@ -38,16 +43,18 @@ function saveTS(){
 and display them as Buttons on the home page.
 Invokes setButtonClick function afterwards to set On-Click attributes for the generated buttons -*/
 function displayButtons(){
-    chrome.storage.sync.get(null, function(result){
-        console.log("in display button:", result);        
-        for(objects in result){
-            var tabObject = result[objects];
-            console.log("tabObject: " , tabObject);
-            var name = tabObject.name;
-            var html = buildButton(name);
-            $('#generateTabs').append(html);   
-        }
-        
+    chrome.storage.local.get(null, function(curTS_Result){
+        chrome.storage.sync.get(null, function(result){
+
+            console.log("in display button:", curTS_Result.currentTabspace); 
+
+            for(objects in result){
+                var tabObject = result[objects];                
+                var name = tabObject.name;
+                var html = buildButton(name, curTS_Result.currentTabspace);
+                $('#generateTabs').append(html);   
+            }        
+        });
     });
     setTimeout(setButtonClick, 1000);
 }
@@ -55,16 +62,20 @@ function displayButtons(){
 function randomSrc(){
     var imageUrls = ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg",
                      "7.jpg","8.jpg","9.jpg","10.jpg","11.jpg","12.jpg","13.jpg","14.jpg","15.jpg"];
-    var randomIndex = Math.floor(Math.random() * imageUrls.length);
-    console.log("randomIndex: ", randomIndex);
-    console.log("imageUrl: ", imageUrls[randomIndex]);
+    var randomIndex = Math.floor(Math.random() * imageUrls.length);    
     return imageUrls[randomIndex];
 }
 
-function buildButton(name){
-     return '<div class="col-xs-4 individualTS"' + 'title="' + name + '">' + '<input type="image"  id="tab-images" src="images/' + randomSrc() + '"/>' + '<p>' + name + '</p>' + '</div>';            
+function buildButton(name, curTS){    
+    console.log(curTS);
+    if (curTS == name){
+        console.log("curTS == name");
+        return '<div class="col-xs-4 individualTS"' + 'title="' + name + '">' + '<input type="image" id="tab-images" src="images/' + randomSrc() + '"/>' + '<p class="curTS" id="title">' + name + '</p>' + '</div>';            
+    } else {
+        return '<div class="col-xs-4 individualTS"' + 'title="' + name + '">' + '<input type="image"  id="tab-images" src="images/' + randomSrc() + '"/>' + '<p id="title">' + name + '</p>' + '</div>';            
+    }
+    
 }
-
 
 
 /*-Function called after timeout to set On-Click attribute to generated Tabspace buttons.
@@ -100,7 +111,7 @@ function displayLinks(name){
                     chrome.tabs.create({url : tempUrl});                           
                 }
               
-                chrome.storage.local.set({currentTabspace : name});
+                chrome.storage.local.set({currentTabspace : name});                
             }
             setTimeout(closeTabs(tabs_to_close), 1000);
         });
@@ -237,6 +248,7 @@ window.onload = function(){
         var removeAddTab = document.createElement('input');
         removeAddTab.setAttribute('type', 'button');
         removeAddTab.setAttribute("value", "x");
+        removeAddTab.setAttribute("class", "btn btn-default btn-xs btn-circle");
         removeAddTab.setAttribute("id", "removeButton");
         removeAddTab.addEventListener('click', function(e) {
             nodeAdd.parentNode.removeChild(nodeAdd);
@@ -267,6 +279,7 @@ window.onload = function(){
         var removeBlockTab = document.createElement('input');
         removeBlockTab.setAttribute('type', 'button');
         removeBlockTab.setAttribute("value", "x");
+        removeBlockTab.setAttribute("class", "btn btn-default btn-xs btn-circle");
         removeBlockTab.setAttribute("id", "removeButton");
         removeBlockTab.addEventListener('click', function(e) {
             nodeBlock.parentNode.removeChild(nodeBlock);
